c5dd1e6ff3e704ad27b25495654d70924c638a86
Younghun Choi <choi0hun@gmail.com>
2013-02-28 08:26:12 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Praiseme::Application.config.secret_key_base = 'f308e626bebef577b6ca14f590e7cea863b0ec5be2ea94e86b4bad090c73debd4d6b2773b25c0201cb6b716ac21a2f76b3d30340ce523ba4eb6291688ed24514'
