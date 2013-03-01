225d276d351077878fbba8786fc139da8d43ef6f
Younghun Choi <choi0hun@gmail.com>
2013-03-02 01:39:10 +0900

    setting nginx unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/praiseme"
+pid "/praiseme/tmp/pids/unicorn.pid"
+stderr_path "/praiseme/unicorn/unicorn.log"
+stdout_path "/praiseme/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
