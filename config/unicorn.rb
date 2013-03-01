c897942dedd006ce7cf26e7fff02e5073d1c7208
Younghun Choi <choi0hun@gmail.com>
2013-03-02 01:43:47 +0900

    fix unicon nginx config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
-working_directory "/praiseme"
-pid "/praiseme/tmp/pids/unicorn.pid"
-stderr_path "/praiseme/unicorn/unicorn.log"
-stdout_path "/praiseme/unicorn/unicorn.log"
+working_directory "/home/pi/praiseme"
+pid "/home/pi/praiseme/tmp/pids/unicorn.pid"
+stderr_path "/home/pi/praiseme/unicorn/unicorn.log"
+stdout_path "/home/pi/praiseme/unicorn/unicorn.log"
 
-listen "/tmp/unicorn.todo.sock"
+listen "/tmp/unicorn.praiseme.sock"
 worker_processes 2
 timeout 30
