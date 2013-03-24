7b22620bd1a3a83823ba8485b7b08a73446461b7
Younghun Choi <choi0hun@gmail.com>
2013-03-24 20:10:02 +0900

    news feed template

--- app/helpers/news_feeds_helper.rb
+++ app/helpers/news_feeds_helper.rb
@@ -1,2 +1,5 @@
 module NewsFeedsHelper
+  def render_news_feed(news_feed)
+    render :partial => "/news_feeds/#{news_feed.notifiable_type.downcase}", :locals => {:news_feed => news_feed}
+  end
 end
