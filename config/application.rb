eb5d4d6d6e3109d9ae14a9f1b307841c8312b9df
Younghun Choi <choi0hun@gmail.com>
2013-05-24 02:02:43 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,8 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Assets should be precompiled for production (so we don't need the gems loaded then)
-Bundler.require(*Rails.groups(assets: %w(development test)))
+#Bundler.require(*Rails.groups(assets: %w(development test)))
+Bundler.require(:default, Rails.env)
 
 module Praiseme
   class Application < Rails::Application
