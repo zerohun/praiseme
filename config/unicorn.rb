8271b594ebff1eb812da959d51294fa30a06e930
Younghun Choi <choi0hun@gmail.com>
2013-06-23 16:22:08 +0900

    new server setting

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+working_directory "/root/praiseme"
+pid "/root/praiseme/tmp/pids/unicorn.pid"
+stderr_path "/root/praiseme/log/unicorn.log"
+stdout_path "/root/praiseme/log/unicorn.log"
+
+listen "/tmp/unicorn.praiseme.sock"
+worker_processes 2
+timeout 30
+
