4a29ff231e14408fbdcd6acc4058ff36a8644f98
Younghun Choi <choi0hun@gmail.com>
2013-07-04 18:28:25 +0900

    compliment with stamp_id and bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -146,4 +146,12 @@ class User < ActiveRecord::Base
   end
 
 
+  def username
+    if self[:username].present?
+      self[:username]
+    else
+      "#{self[:first_name]} #{self[:last_name]}"
+    end
+  end
+
 end
