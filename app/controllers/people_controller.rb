1ac7fdce0473485cfb35018dcc11654e358da019
ggomagundan <pbs52@hanmail.net>
2013-07-04 12:04:09 +0900

    make user search using where syntax (must chagen to sphinx engine)

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,12 @@
 class PeopleController < ApplicationController
   def index
     if params[:q].present?
-      @friends = User.search(params[:q], 
-                             :sql => {:joins => :sns_connections, :select => "users.*, sns_connections.uid as uid" },
-                             :page => params[:page],
-                             :per_page => 15
-                            )
+      @friends = User.where("username like '%#{params[:q]}%'").page(params[:page]).per(15)
+    #  @friends = User.search(params[:q], 
+    #                        :sql => {:joins => :sns_connections, :select => "users.*, sns_connections.uid as uid" },
+    #                         :page => params[:page],
+    #                         :per_page => 15
+    #                        )
     else
       @friends = current_user.friends.reorder("status desc").page(params[:page]).per(15)
     end
