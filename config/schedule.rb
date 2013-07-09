c2bafa87ad6eb86249fecbdc7050526076342dea
ggomagundan <pbs52@hanmail.net>
2013-07-09 13:20:36 +0900

    adding stamp index schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,3 +7,7 @@ end
 every 1.day , :at => "1:30 am" do
   rake "user_stamp:delete_empty_user_stamp", :output => "log/empty_user_stamp.log", :environment => ENV['RAILS_ENV']
 end
+
+every 1.day , :at => "1:20 am" do
+  rake "search_suggestion:stamp_index", :output => "log/stamp_indexing_log.log", :environment => ENV['RAILS_ENV']
+end
