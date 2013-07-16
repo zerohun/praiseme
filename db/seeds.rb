d410704cbf2880d055e71dec16cbd794d5796d4b
Younghun Choi <choi0hun@gmail.com>
2013-07-16 18:51:47 +0900

    default trophy image seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+def uploaded_png_file(path, file_name)
+  ActionDispatch::Http::UploadedFile.new :filename => file_name, 
+                                          :type => "image/png",
+                                          :tempfile => File.open(path, 'r')
+
+end
+
+
+DefaultTrophyImage.create :file => uploaded_png_file("#{Rails.root}/public/default_trophy_images_seed/thumb_badge.png", "thumb_badge.png")
+DefaultTrophyImage.create :file => uploaded_png_file("#{Rails.root}/public/default_trophy_images_seed/medal_badge.png", "medal_badge.png")
+DefaultTrophyImage.create :file => uploaded_png_file("#{Rails.root}/public/default_trophy_images_seed/trophy_badge.png", "trophy_badge.png")
+
+
