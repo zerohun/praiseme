2476712c4799ecbe37bab06ed965e6a7e981e6ba
ggomagundan <pbs52@hanmail.net>
2013-07-16 17:26:33 +0900

    make comment admin page

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,37 @@
+class Admin::CommentsController < Admin::ApplicationController
+
+
+  before_filter :is_admin_login
+ 
+  def index
+    @comments = Comment.page(params[:page]).per(20)
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(comment_params)
+      redirect_to [:admin, @comment], :notice  => "Successfully updated comment."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to admin_comments_url, :notice => "Successfully destroyed comment."
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
