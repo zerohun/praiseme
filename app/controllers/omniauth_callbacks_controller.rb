52ba8425764b431a95bd6e731865c48c5df8312f
Younghun Choi <choi0hun@gmail.com>
2013-07-17 16:08:39 +0900

    bug fix

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,6 +8,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sns_connection.save
       user.from_omniauth(auth)
       user.save if user.changed
+      user = user.reload
+      user.delay.invites_friends_automatically if sns_connection.has_invited_friends == false
       user.delay.invites_friends_automatically if sns_connection.has_invited_friends == false
       sign_in_and_redirect user, notice: "Signed in!"
     else
