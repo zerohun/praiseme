645ea0721529864bd766f8e1b6258d65e29ac7be
ggomagundan <pbs52@hanmail.net>
2013-08-12 19:09:51 +0900

    add follower all people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,20 +2,25 @@ class PeopleController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     if current_user.present?
-      if params[:q].present?
-        @friends = current_user.friends.where("users.username like ? or concat(users.first_name, users.last_name) like ?", "%#{params[:q]}%", "%#{params[:q]}%")
+      @friends = Following.where(:follower_id => current_user.id)
+     if params[:all].present?
+       @friends = User.where.not(:id => current_user.id)
+     end
+     if params[:q].present?
+        @friends = @friends.where("users.username like ? or concat(users.first_name, users.last_name) like ?", "%#{params[:q]}%", "%#{params[:q]}%")
       #  @friends = User.search(params[:q], 
       #                        :sql => {:joins => :sns_connections, :select => "users.*, sns_connections.uid as uid" },
       #                         :page => params[:page],
       #                         :per_page => 15
       #                        )
+     
       else
-        @friends = current_user.friends.reorder("status desc")
+        #@friends = @friends.reorder("status desc")
       end
-      @friends_count = @friends.count
+     # @friends_count = @friends.count
     else
-      @friends_count = User.count
-      @friends = User.joins("left outer join user_stamps on users.id = user_stamps.user_id").group("users.id").select("users.*, sum(user_stamps.score) as total_score").reorder("total_score desc")
+     # @friends_count = User.count
+      #@friends = User.joins("left outer join user_stamps on users.id = user_stamps.user_id").group("users.id").select("users.*, sum(user_stamps.score) as total_score").reorder("total_score desc")
     end
     @friends = @friends.page(params[:page]).per(15)
   end
