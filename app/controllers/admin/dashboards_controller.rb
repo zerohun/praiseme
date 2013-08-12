e445b119ea584b6b3622bdddb83117e2f4a5b044
ggomagundan <pbs52@hanmail.net>
2013-08-12 12:29:30 +0900

    dash board

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,13 +3,24 @@ class Admin::DashboardsController < Admin::ApplicationController
 
     now_month = Date.today.month
     @compliment = Compliment.where('extract(month from created_at) = ?' , now_month)
-    @new_compliment = Compliment.where("updated_at >= ? and updated_At < ?",Date.yesterday.to_time, Date.today.to_time)
-    @new_user = User.where("users.updated_at >= ? and users.updated_At < ?",Date.yesterday.to_time, Date.today.to_time)
-    @new_stamp = Stamp.where("updated_at >= ? and updated_At < ?",Date.yesterday.to_time, Date.today.to_time)
-    @new_user_stamp = UserStamp.where("updated_at >= ? and updated_At < ?",Date.yesterday.to_time, Date.today.to_time)
-    @new_comment = Comment.where("updated_at >= ? and updated_At < ?",Date.yesterday.to_time, Date.today.to_time)
+  
+    @new_compliment = []
+    @new_user = []
+    @new_stamp = []
+    @new_user_stamp = []
+    @new_comment = []
 
+    (1..7).each do |i| 
+      pre_day = (Date.today - (i-1).days).to_time 
+      more_pre_day = (Date.today - i.days).to_time 
+      
+      @new_compliment.push(Compliment.where("updated_at >= ? and updated_At < ?",more_pre_day,pre_day))
+      @new_user.push(User.where("users.updated_at >= ? and users.updated_At < ?",more_pre_day,pre_day))
+      @new_stamp.push(Stamp.where("updated_at >= ? and updated_At < ?",more_pre_day, pre_day))
+      @new_user_stamp.push(UserStamp.where("updated_at >= ? and updated_At < ?",more_pre_day, pre_day))
+      @new_comment.push(Comment.where("updated_at >= ? and updated_At < ?",more_pre_day, pre_day))
 
+    end
   end
 
   def show
