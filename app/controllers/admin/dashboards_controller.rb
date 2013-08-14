37d2998b87bd5dafaf7ec0439fe1ab1680951ea2
ggomagundan <pbs52@hanmail.net>
2013-08-14 18:23:01 +0900

    modify bug

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -5,15 +5,17 @@ class Admin::DashboardsController < Admin::ApplicationController
     @compliment = Compliment.where('extract(month from created_at) = ?' , now_month)
  
     if params[:new_user].present?
-      pre_day = (Date.today - 0.days).to_time 
+      pre_day = Time.now 
       more_pre_day = (Date.today - 6.days).to_time 
-      @new_user=User.where("users.updated_at >= ? and users.updated_at < ? and status = 1",more_pre_day,pre_day)
-      @new_user = @new_user.order('updated_at desc')
+      @new_user=User.where("joined_at >= ? and joined_at < ? and status = 1",more_pre_day,pre_day)
+      @new_user = @new_user.order('joined_at desc')
       
     
     else
+
       @new_compliment = []
       @new_user = []
+      @new_active_user = []
       @new_stamp = []
       @new_user_stamp = []
       @new_comment = []
@@ -27,11 +29,12 @@ class Admin::DashboardsController < Admin::ApplicationController
           more_pre_day = (Date.today - i.days).to_time 
         end
 
-        @new_compliment.push(Compliment.where("updated_at >= ? and updated_at < ?",more_pre_day,pre_day))
-        @new_user.push(User.where("users.updated_at >= ? and users.updated_at < ?",more_pre_day,pre_day))
-        @new_stamp.push(Stamp.where("updated_at >= ? and updated_at < ?",more_pre_day, pre_day))
-        @new_user_stamp.push(UserStamp.where("updated_at >= ? and updated_at < ?",more_pre_day, pre_day))
-        @new_comment.push(Comment.where("updated_at >= ? and updated_at < ?",more_pre_day, pre_day))
+        @new_compliment.push(Compliment.where("updated_at >= ? and updated_at < ?",more_pre_day,pre_day).count)
+        @new_user.push(User.where("users.updated_at >= ? and users.updated_at < ?",more_pre_day,pre_day).count)
+        @new_active_user.push(User.where("users.joined_at >= ? and users.joined_at < ? and users.status = 1",more_pre_day,pre_day).count)
+        @new_stamp.push(Stamp.where("updated_at >= ? and updated_at < ?",more_pre_day, pre_day).count)
+        @new_user_stamp.push(UserStamp.where("updated_at >= ? and updated_at < ?",more_pre_day, pre_day).count)
+        @new_comment.push(Comment.where("updated_at >= ? and updated_at < ?",more_pre_day, pre_day).count)
 
       end
 
