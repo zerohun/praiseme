6903efae91345b4d0e3a124cfbd9d1b88b07a03d
Younghun Choi <choi0hun@gmail.com>
2013-08-22 18:26:55 +0900

    bug fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @is_user_stamps = current_user.user_stamps.reorder("user_stamps.score desc").limit(2)
-    @has_user_stamps = current_user.user_stamps.reorder("user_stamps.score desc").limit(2)
+    @is_user_stamps = current_user.user_stamps.where("stamps.verb = ?", 0).reorder("user_stamps.score desc").limit(2)
+    @has_user_stamps = current_user.user_stamps.where("stamps.verb = ?", 1).reorder("user_stamps.score desc").limit(2)
   end
 end
