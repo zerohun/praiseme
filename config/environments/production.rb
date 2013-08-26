9127e3dc6a1737b3ecaa72a4f958249409f5fd42
Younghun Choi <choi0hun@gmail.com>
2013-08-26 15:47:08 +0900

    bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Praiseme::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "www.startglory.com" }
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
